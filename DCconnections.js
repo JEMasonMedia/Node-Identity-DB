9ead2717b2141b86cc81c3e4725a6c35886dc41a
JEMasonMedia <42191306+JEMasonMedia@users.noreply.github.com>
2022-04-27 10:47:47 -0500

    .

--- DCconnections.js
+++ DCconnections.js
@@ -29,27 +29,27 @@ await NIDB.useDatabase(
   }
 )
 
-// await NIDB.useDatabase(
-//   'items',
-//   'MYSQLDB',
-//   {
-//     host: process.env.MYSQL_HOST,
-//     port: process.env.MYSQL_PORT,
-//     user: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database: process.env.MYSQL_DATABASE,
-//   },
-//   {},
-//   (err, dbConn) => {
-//     if (!err && dbConn) {
-//       console.log(
-//         `The ${dbConn.databaseType} connection: '${dbConn.connectionName}', on host '${dbConn.connectionConfig.host}', has connected successfully!`
-//       )
-//     } else {
-//       console.log(err)
-//     }
-//   }
-// )
+await NIDB.useDatabase(
+  'items',
+  'MYSQLDB',
+  {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  },
+  {},
+  (err, dbConn) => {
+    if (!err && dbConn) {
+      console.log(
+        `The ${dbConn.databaseType} connection: '${dbConn.connectionName}', on host '${dbConn.connectionConfig.host}', has connected successfully!`
+      )
+    } else {
+      console.log(err)
+    }
+  }
+)
 
 // DBconnections['items'] = { dbid: NIDB.useDatabase('mysql', 'mysql') }
 // DBconnections['cart'] = { dbid: NIDB.useDatabase('mssql', 'mssql') }
--- Index.js
+++ Index.js
@@ -3,13 +3,13 @@ import './DCconnections.js'
 import Models from './Models.js'
 
 NIDB.useModel('users', 'users', Models['users'])
-// NIDB.useModel('items', 'items', Models['items'])
+NIDB.useModel('items', 'items', Models['items'])
 
 // const user = DBconnections['users'].select('*').where({ id: 1 }).first()
 // const item = DBconnections['items'].select('*').where({ id: 1 }).first()
 
 console.log(NIDB.databaseConnections['users'].models['users'].model.getModel())
-// console.log(NIDB.databaseConnections['items'].models['items'].model.getModel())
+console.log(NIDB.databaseConnections['items'].models['items'].model.getModel())
 
 NIDB.closeConnections(null, (err, dbList) => {
   if (!err && dbList) {
